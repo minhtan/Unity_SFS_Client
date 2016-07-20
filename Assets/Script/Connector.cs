3257a6ad1c1df1d90e038953bb927a441a2e5b09
Tan <m.tan2007@gmail.com>
2016-07-20 17:28:02 +0700

    working on login

--- Assets/Script/Connector.cs
+++ Assets/Script/Connector.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using Sfs2X;
 using Sfs2X.Util;
 using Sfs2X.Core;
+using Sfs2X.Logging;
+using System.Collections.Generic;
+using Sfs2X.Entities;
 
 public class Connector : MonoBehaviour {
 	
@@ -15,7 +18,7 @@ public class Connector : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Connect ();
+		_Connect ();
 	}
 	
 	// Update is called once per frame
@@ -24,13 +27,21 @@ public class Connector : MonoBehaviour {
 			sfs.ProcessEvents();
 	}
 
-	public void Connect(){
+	#region connection
+
+	public void _Connect(){
 		if (sfs == null || !sfs.IsConnected) {
 			sfs = new SmartFox ();
 			sfs.ThreadSafeMode = true;
 
 			sfs.AddEventListener (SFSEvent.CONNECTION, OnConnection);
 			sfs.AddEventListener (SFSEvent.CONNECTION_LOST, OnConnectionLost);
+			sfs.AddEventListener (SFSEvent.CONNECTION_RETRY, OnConnectionRetry);
+			sfs.AddEventListener (SFSEvent.CONNECTION_RESUME, OnConnectionResume);
+
+			sfs.AddLogListener(LogLevel.INFO, OnInfoMessage);
+			sfs.AddLogListener(LogLevel.WARN, OnWarnMessage);
+			sfs.AddLogListener(LogLevel.ERROR, OnErrorMessage);
 
 			ConfigData cfg = new ConfigData ();
 			cfg.Host = host;
@@ -43,26 +54,83 @@ public class Connector : MonoBehaviour {
 		}
 	}
 
-	public void Disconnect(){
+	public void _Disconnect(){
 		sfs.Disconnect();
+	}
+
+	public void _KillConnection(){
+		sfs.KillConnection ();
 	}
 
 	void OnConnection(BaseEvent evt){
 		if ((bool)evt.Params ["success"]) {
-			Debug.Log ("Connected");
+			Debug.Log ("Connected" + evt.Params.Count);
 		} else {
 			reset ();
 		}
 	}
 
 	void OnConnectionLost(BaseEvent evt){
+		Debug.Log ("Disconnected " + (string)evt.Params["reason"]);
 		reset ();
 	}
 
-	void reset(){
-		sfs.RemoveEventListener(SFSEvent.CONNECTION, OnConnection);
-		sfs.RemoveEventListener(SFSEvent.CONNECTION_LOST, OnConnectionLost);
+	void OnConnectionRetry(BaseEvent evt){
+		Debug.Log ("Connection loss, attemping to reconnect");
+	}
 
+	void OnConnectionResume(BaseEvent evt){
+		Debug.Log ("Connection resumed");
+	}
+
+	public void OnInfoMessage(BaseEvent evt) {
+		string message = (string)evt.Params["message"];
+		ShowLogMessage("INFO", message);
+	}
+
+	public void OnWarnMessage(BaseEvent evt) {
+		string message = (string)evt.Params["message"];
+		ShowLogMessage("WARN", message);
+	}
+
+	public void OnErrorMessage(BaseEvent evt) {
+		string message = (string)evt.Params["message"];
+		ShowLogMessage("ERROR", message);
+	}
+
+	private void ShowLogMessage(string level, string message) {
+		message = "[SFS > " + level + "] " + message;
+		Debug.Log(message);
+	}
+
+	#endregion
+
+	#region login
+
+	public void _Login(){
+		sfs.AddEventListener(SFSEvent.LOGIN, OnLogin);
+		sfs.AddEventListener(SFSEvent.LOGIN_ERROR, OnLoginError);
+
+		sfs.Send (new Sfs2X.Requests.LoginRequest ("test1","123456",zone));
+	}
+
+	void OnLogin(BaseEvent evt){
+		User user = (User) evt.Params["user"];
+		Debug.Log ("Logged in " + user.Name);
+		evt.Params.GetParamKeys ().DebugList ();
+	}
+
+	void OnLoginError(BaseEvent evt){
+		sfs.Disconnect ();
+		Debug.Log ("Log in failed");
+		evt.Params.GetParamKeys ().DebugList ();
+	}
+
+	#endregion
+
+
+	void reset(){
+		sfs.RemoveAllEventListeners ();
 		sfs = null;
 	}
 }
---
+++ Assets/Script/Util.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Util {
+
+	public static List<string> GetParamKeys(this IDictionary param){
+		List<string> list =  new List<string>();
+
+		foreach(string value in param.Keys){
+			list.Add (value);
+		}
+
+		return list;
+	}
+
+	public static void DebugList(this List<string> list){
+		foreach(string value in list){
+			Debug.Log (value);
+		}
+	}
+}
